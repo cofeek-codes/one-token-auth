20ba5c23ee9d65a41199b83b5efe625c93d33c90
cofeek-codes <kormyshev11@mail.ru>
2022-06-21 10:39:55 +0500

    reg

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, Post } from '@nestjs/common'
+
+import { AuthService } from './auth.service'
+import { RegisterDTO } from './dto/register.dto'
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+	constructor(private readonly authService: AuthService) {}
+	@Post('/register')
+	async register(@Body() dto: RegisterDTO) {
+		return await this.authService.register(dto)
+	}
 }
